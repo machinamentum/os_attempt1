2dfeebf5961c182dbe1d73070b266e7cf1ccd8fd
Joshua Huelsman <joshuahuelsman@gmail.com>
2018-06-10 18:59:07 -0400

    Update build.bat to use i686-elf-gcc toolchain since clang causes us to have incorrect behavior.

--- build.bat
+++ build.bat
@@ -1,4 +1,3 @@
 nasm -felf32 boot.s -o boot.o
-D:\LLVM\bin\clang -target i686-none-elf -c main.cpp -o kernel.o -std=c++11 -ffreestanding -O2 -Wall -Wextra 
-
-D:\LLVM\bin\lld -flavor gnu -T linker.ld -o myos.bin -O2 -nostdlib boot.o kernel.o 
+i686-elf-gcc -c main.cpp -o kernel.o -std=c++11 -ffreestanding -O2 -Wall -Wextra 
+i686-elf-ld -T linker.ld -o myos.bin -O2 -nostdlib boot.o kernel.o 
