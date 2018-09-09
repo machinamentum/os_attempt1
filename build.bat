644c911517a25d1ee59ceed68545a2b1621c2dc6
Joshua Huelsman <joshuahuelsman@gmail.com>
2018-09-09 02:09:00 -0400

    House keeping.

--- build.bat
+++ build.bat
@@ -1,11 +1,14 @@
 SET TOOLCHAIN=%~dp0\i686-elf-tools\bin\
 
-SET COMMON_FLAGS=-std=c++11 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -funsigned-char
+SET INCLUDE_PATHS=-Iinclude
+SET COMMON_FLAGS=%INCLUDE_PATHS% -std=c++11 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -funsigned-char
 
-%TOOLCHAIN%\nasm -felf32 boot.s -o boot.o
-%TOOLCHAIN%\i686-elf-gcc -c main.cpp -o main.o             %COMMON_FLAGS%
-%TOOLCHAIN%\i686-elf-gcc -c interrupts.cpp -o interrupts.o %COMMON_FLAGS% -mgeneral-regs-only
-%TOOLCHAIN%\i686-elf-gcc -c vga.cpp -o vga.o               %COMMON_FLAGS%
-%TOOLCHAIN%\i686-elf-gcc -c heap.cpp -o heap.o             %COMMON_FLAGS%
+%TOOLCHAIN%\nasm -felf32    src\boot.s         -o boot.o
+%TOOLCHAIN%\i686-elf-gcc -c src\main.cpp       -o main.o       %COMMON_FLAGS%
+%TOOLCHAIN%\i686-elf-gcc -c src\interrupts.cpp -o interrupts.o %COMMON_FLAGS% -mgeneral-regs-only
+%TOOLCHAIN%\i686-elf-gcc -c src\vga.cpp        -o vga.o        %COMMON_FLAGS%
+%TOOLCHAIN%\i686-elf-gcc -c src\heap.cpp       -o heap.o       %COMMON_FLAGS%
 
 %TOOLCHAIN%\i686-elf-ld -T linker.ld -o myos.bin -O2 -static -nostdlib boot.o main.o interrupts.o vga.o heap.o
+
+del *.o
--- build.sh
+++ build.sh
@@ -2,10 +2,14 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 export PATH="$SCRIPTPATH/toolchain/bin:$PATH"
 nasm -felf32 boot.s -o boot.o
 
-COMMON_FLAGS="-std=c++11 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -funsigned-char"
-i686-elf-gcc -c main.cpp       -o main.o       $COMMON_FLAGS
-i686-elf-gcc -c interrupts.cpp -o interrupts.o $COMMON_FLAGS -mgeneral-regs-only
-i686-elf-gcc -c vga.cpp        -o vga.o        $COMMON_FLAGS
-i686-elf-gcc -c heap.cpp       -o heap.o       $COMMON_FLAGS
+INCLUDE_PATHS=-Iinclude
+COMMON_FLAGS="$INCLUDE_PATHS -std=c++11 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -funsigned-char"
 
-i686-elf-ld -T linker.ld -o myos.bin -O2 -nostdlib boot.o main.o interrupts.o vga.o
+i686-elf-gcc -c src/main.cpp       -o main.o       $COMMON_FLAGS
+i686-elf-gcc -c src/interrupts.cpp -o interrupts.o $COMMON_FLAGS -mgeneral-regs-only
+i686-elf-gcc -c src/vga.cpp        -o vga.o        $COMMON_FLAGS
+i686-elf-gcc -c src/heap.cpp       -o heap.o       $COMMON_FLAGS
+
+i686-elf-ld -T linker.ld -o myos.bin -O2 -nostdlib boot.o main.o interrupts.o vga.o heap.o
+
+rm *.o
