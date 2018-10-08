69e4209205676cfb415f292d804b68a903cf3deb
Joshua Huelsman <joshuahuelsman@gmail.com>
2018-10-08 01:53:36 -0400

    Update Linux build file. Tweak VMWare VGA2 driver to only send update command on entire screen.

--- build.sh
+++ build.sh
@@ -5,11 +5,14 @@ nasm -felf32 src/boot.s -o boot.o
 INCLUDE_PATHS=-Iinclude
 COMMON_FLAGS="$INCLUDE_PATHS -std=c++11 -ffreestanding -O2 -Wall -Wextra -Wno-write-strings -funsigned-char"
 
-i686-elf-gcc -c src/main.cpp       -o main.o       $COMMON_FLAGS
-i686-elf-gcc -c src/interrupts.cpp -o interrupts.o $COMMON_FLAGS -mgeneral-regs-only
-i686-elf-gcc -c src/vga.cpp        -o vga.o        $COMMON_FLAGS
-i686-elf-gcc -c src/heap.cpp       -o heap.o       $COMMON_FLAGS
+i686-elf-gcc -c src/main.cpp         -o main.o         $COMMON_FLAGS
+i686-elf-gcc -c src/interrupts.cpp   -o interrupts.o   $COMMON_FLAGS -mgeneral-regs-only
+i686-elf-gcc -c src/vga.cpp          -o vga.o          $COMMON_FLAGS
+i686-elf-gcc -c src/heap.cpp         -o heap.o         $COMMON_FLAGS
+i686-elf-gcc -c src/ide.cpp          -o ide.o          $COMMON_FLAGS
+i686-elf-gcc -c src/vmware_svga2.cpp -o vmware_svga2.o $COMMON_FLAGS
+i686-elf-gcc -c src/math.cpp         -o math.o         $COMMON_FLAGS
 
-i686-elf-ld -T linker.ld -o myos.bin -O2 -nostdlib boot.o main.o interrupts.o vga.o heap.o
+i686-elf-ld -T linker.ld -o myos.bin -O2 -nostdlib boot.o main.o interrupts.o vga.o heap.o ide.o vmware_svga2.o math.o
 
 rm *.o
